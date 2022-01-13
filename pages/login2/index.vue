8630e696af96a511739bb9dd7ec2179560200c0c
Saikat Biswas <saikat1236@gmail.com>
2022-01-13 17:21:01 +0530

    wr888686

--- pages/login2/index.vue
+++ pages/login2/index.vue
@@ -51,7 +51,7 @@ export default {
         .signInWithEmailAndPassword(this.email, this.password)
         .then(data => {
           console.log(data)
-          this.$router.replace({ path: 'evolve/evolveII' })
+          this.$router.replace({ name: 'evolveII' })
         })
         .catch(error => {
           this.error = error
